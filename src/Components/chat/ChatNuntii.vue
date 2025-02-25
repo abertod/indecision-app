c129d1595fea13f688dbbfd892a3f007635e59e1
master <master@dib-e026-imac20.dib.upv.es>
2025-02-25 20:18:42 +0100

    Componentes del chat

---
+++ src/Components/chat/ChatNuntii.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4 flex-1 overflow-y-auto">
+    <div class="flex flex-col space-y-2">
+      <div class="flex justify-end">
+        <div class="bg-blue-200 text-black p-2 max-w-xs rounded-xl">¿Cuándo nos vemos?</div>
+      </div>
+
+      <div class="flex">
+        <div class="bg-yellow-300 text-black p-2 max-w-xs rounded-xl">Eso te iba a preguntar</div>
+      </div>
+    </div>
+  </div>
+</template>
