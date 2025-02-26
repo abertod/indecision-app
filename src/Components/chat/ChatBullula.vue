8c201cb5eeda4396ee30cc9ecda7cf60b233b691
master <master@dib-e026-imac20.dib.upv.es>
2025-02-26 16:42:39 +0100

    Api si no

---
+++ src/Components/chat/ChatBullula.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="meusEst" class="flex justify-end">
+    <div class="bg-blue-200 text-black p-2 max-w-xs rounded-xl">{{ nuntius }}</div>
+  </div>
+
+  <div v-else class="flex">
+    <div class="bg-yellow-300 text-black p-2 max-w-xs rounded-xl">
+      <span class="capitalize">{{ nuntius }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  nuntius: string;
+  meusEst: boolean;
+  imago?: string;
+}
+
+defineProps<Props>();
+</script>
--- src/Components/chat/ChatNuntii.vue
+++ src/Components/chat/ChatNuntii.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="p-4 flex-1 overflow-y-auto">
     <div class="flex flex-col space-y-2">
-      <div class="flex justify-end">
-        <div class="bg-blue-200 text-black p-2 max-w-xs rounded-xl">¿Cuándo nos vemos?</div>
-      </div>
-
-      <div class="flex">
-        <div class="bg-yellow-300 text-black p-2 max-w-xs rounded-xl">Eso te iba a preguntar</div>
-      </div>
+      <ChatBullula :meus-est="true" nuntius="Cuando nos vemos?" />
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import ChatBullula from './ChatBullula.vue';
+</script>
