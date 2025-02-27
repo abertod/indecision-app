d17473881b0ca07859061dd19aaa228b8f01a268
master <master@dib-e026-imac20.dib.upv.es>
2025-02-27 19:04:45 +0100

    Chat contesta con gif SI o NO

--- src/Components/chat/ChatNuntii.vue
+++ src/Components/chat/ChatNuntii.vue
@@ -2,6 +2,12 @@
   <div class="p-4 flex-1 overflow-y-auto">
     <div class="flex flex-col space-y-2">
       <ChatBullula :meus-est="true" nuntius="Cuando nos vemos?" />
+
+      <ChatBullula
+        :meus-est="false"
+        nuntius="No"
+        imago="https://yesno.wtf/assets/no/10-d5ddf3f82134e781c1175614c0d2bab2.gif"
+      />
     </div>
   </div>
 </template>
